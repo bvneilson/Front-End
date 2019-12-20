634c5633103b36c89142767b791d8c03a23420ce
Neilson <Brennan.Neilson@henryschein.com>
2019-12-19 20:37:36 -0700

    Starting on Header component and some misc

---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import { HeaderContext } from '../contexts/HeaderContext.js';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <HeaderContext.Consumer>
+    {({loggedIn}) => (
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h5" className={classes.title}>
+            Dad Jokes
+          </Typography>
+          <Button color="inherit">{alert(loggedIn)}</Button>
+          <Button color="inherit">Sign Up</Button>
+        </Toolbar>
+      </AppBar>
+    )}
+    </HeaderContext.Consumer>
+  )
+}
+
+export default Header;
