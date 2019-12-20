dcb322423b1875ea1f661b6e7c9efbb69c8e7c85
Brennan Neilson <bvneilson@gmail.com>
2019-12-19 21:15:38 -0700

    Updating title, favicon, description and starting on login form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 import Container from '@material-ui/core/Container';
 
 
 import Header from './components/Header.js';
+import Login from './components/Login.js';
 
 function App() {
   return (
     <Container className="App">
       <Header />
+      <Route path="/login" component={Login} />
     </Container>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,6 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-import { HeaderContext } from '../contexts/HeaderContext.js';
-
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
@@ -25,22 +23,18 @@ const Header = () => {
   const classes = useStyles();
 
   return (
-    <HeaderContext.Consumer>
-    {({loggedIn}) => (
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h5" className={classes.title}>
-            Dad Jokes
-          </Typography>
-          <Button color="inherit">{alert(loggedIn)}</Button>
-          <Button color="inherit">Sign Up</Button>
-        </Toolbar>
-      </AppBar>
-    )}
-    </HeaderContext.Consumer>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h5" className={classes.title}>
+          Dad Jokes
+        </Typography>
+        <Button color="inherit">Log In</Button>
+        <Button color="inherit">Sign Up</Button>
+      </Toolbar>
+    </AppBar>
   )
 }
 
